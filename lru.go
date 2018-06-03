d6d1f66d9c10652359ed705e42a65b000ebeea5a
ytanko <fravia190@gmail.com>
2018-06-03 17:02:01 +0300

    Initial commit

---
+++ lru.go
@@ -0,0 +1,27 @@
+package main
+
+//LRUCache main struct
+type LRUCache struct {
+}
+
+//Constructor for LRU
+func Constructor(capacity int) (l LRUCache) {
+	return l
+}
+
+//Get key from Cache
+func (l *LRUCache) Get(key int) int {
+	return -1
+}
+
+//Put key, value pain in Cache
+func (l *LRUCache) Put(key int, value int) {
+
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
